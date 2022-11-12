579ad2b1bdaaf024af9791e9ffe368c5dd5150cc
CinthiaElena <60494641+CinthiaElena@users.noreply.github.com>
2022-11-12 00:50:01 -0600

    Creación de views para las urls agregadas
    Vistas creadas:
    - profile
    - profileConfig
    - courses
    - jobs

--- coursesapp/urls.py
+++ coursesapp/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
 	path('register/', views.registerPage, name="register"), # Se crea la URL de registro que tendrá lo que la view RegisterPage
 	path('login/', views.loginPage, name="login"),  #Se crea la URL de login que tendrá lo que la view LoginPage
 	path('logout/', views.logoutUser, name="logout"), 
-
+	path('profile/', views.profileUser, name='profile'),
+	path('profileConfig/', views.profileConfig, name='profileConfig'),
+	path('courses/', views.courses, name='courses'),
+	path('jobs/', views.jobs, name='jobs'),
     path('', views.home, name="home") #Se crea la URL de home que tendrá lo que la view home
 
 
