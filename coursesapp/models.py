2b2e14921fd3592cd889c28b33857a6febe54550
Gerardo Felix <gera17_04@hotmail.com>
2022-11-12 22:28:43 -0700

    model Course added

--- coursesapp/models.py
+++ coursesapp/models.py
@@ -9,3 +9,16 @@ class Perfil(models.Model):
     # Python 3
     def __str__(self): 
         return self.usuario.username
+
+class Course(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    title = models.CharField(max_length=100)
+    price = models.FloatField(max_length=10)
+    url = models.URLField(max_length=255)
+    category = models.CharField(max_length=50)
+    rating = models.FloatField(max_length=5)
+    language = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.title
+    
