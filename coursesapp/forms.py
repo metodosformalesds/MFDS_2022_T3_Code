eaa2cd3d6ae40840374d9075cadcaaa5e6f0d1e2
CinthiaElena <60494641+CinthiaElena@users.noreply.github.com>
2022-11-21 17:23:31 -0600

    Creación de formulario de imagen de perfil.

--- coursesapp/forms.py
+++ coursesapp/forms.py
@@ -3,6 +3,7 @@ Este archivo contiene los formularios creados para el registro
 """
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Perfil
 
 from django import forms
 
@@ -11,3 +12,8 @@ class CreateUserForm(UserCreationForm): #Se define el formulario de registro
 		model = User #Se utiliza el modelo de Usuario ya predefinido de la base de datos
 		fields = ['username', 'email', 'password1', 'password2'] #Se ingresan las variables a utilizar en el formulario de registro
 
+class UpdateUserFormAvatar(forms.ModelForm):
+
+    class Meta:
+        model = Perfil
+        fields = ['avatar']
