8d2c84fdfd0943efb117b7421ffec968abd10bc2
CinthiaElena <60494641+CinthiaElena@users.noreply.github.com>
2022-11-23 17:37:30 -0600

    Creación de formularios para skills.

--- coursesapp/forms.py
+++ coursesapp/forms.py
@@ -3,7 +3,7 @@ Este archivo contiene los formularios creados para el registro
 """
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Perfil
+from .models import Perfil, Skills
 from django.contrib.auth import get_user_model
 
 from django import forms
@@ -42,3 +42,67 @@ class SetPasswordForm(SetPasswordForm):
     class Meta:
         model = get_user_model()
         fields = ['new_password1', 'new_password2']
+
+#SoftSkills
+class SoftSkill1_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['soft_skill1']
+
+class SoftSkill2_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['soft_skill2']
+
+class SoftSkill3_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['soft_skill3']
+
+class SoftSkill4_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['soft_skill4']
+
+class SoftSkill5_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['soft_skill5']
+
+
+
+#HardSkills
+class HardSkill1_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['hard_skill1']
+
+class HardSkill2_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['hard_skill2']
+
+class HardSkill3_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['hard_skill3']
+
+class HardSkill4_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['hard_skill4']
+
+class HardSkill5_form(forms.ModelForm):
+
+    class Meta:
+        model = Skills
+        fields = ['hard_skill5']
